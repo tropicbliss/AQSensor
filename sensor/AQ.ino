39c74a08266e55791b4e67c0701fa52934b7201b
tropicbliss <tropicbliss@protonmail.com>
2023-09-16 00:59:04 +0800

    Update AQ.ino

--- sensor/AQ.ino
+++ sensor/AQ.ino
@@ -1,19 +1,30 @@
 /*
 Important: This code is only for the DIY PRO PCB Version 3.7 that has a push button mounted.
-This is the code for the AirGradient DIY PRO Air Quality Sensor with an ESP8266 Microcontroller.
+
+This is the code for the AirGradient DIY PRO Air Quality Sensor with an ESP8266 Microcontroller with the SGP40 TVOC module from AirGradient.
+
 It is a high quality sensor showing PM2.5, CO2, Temperature and Humidity on a small display and can send data over Wifi.
+
 Build Instructions: https://www.airgradient.com/open-airgradient/instructions/diy-pro-v37/
+
 Kits (including a pre-soldered version) are available: https://www.airgradient.com/open-airgradient/kits/
+
 The codes needs the following libraries installed:
 “WifiManager by tzapu, tablatronix” tested with version 2.0.11-beta
 “U8g2” by oliver tested with version 2.32.15
+"Sensirion I2C SGP41" by Sensation Version 0.1.0
+"Sensirion Gas Index Algorithm" by Sensation Version 3.2.1
+
 Configuration:
 Please set in the code below the configuration parameters.
+
 If you have any questions please visit our forum at https://forum.airgradient.com/
+
 If you are a school or university contact us for a free trial on the AirGradient platform.
 https://www.airgradient.com/
+
 CC BY-SA 4.0 Attribution-ShareAlike 4.0 International License
-Code modified by tropicbliss
+
 */
 
 
@@ -22,21 +33,39 @@ Code modified by tropicbliss
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+
 #include <EEPROM.h>
+
+//#include "SGP30.h"
+#include <SensirionI2CSgp41.h>
+#include <NOxGasIndexAlgorithm.h>
+#include <VOCGasIndexAlgorithm.h>
+
+
 #include <U8g2lib.h>
 
 AirGradient ag = AirGradient();
+SensirionI2CSgp41 sgp41;
+VOCGasIndexAlgorithm voc_algorithm;
+NOxGasIndexAlgorithm nox_algorithm;
+// time in seconds needed for NOx conditioning
+uint16_t conditioning_s = 10;
 
 // for peristent saving and loading
 int addr = 4;
+byte value;
 
 // Display bottom right
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
+// Replace above if you have display on top left
+//U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R2, /* reset=*/ U8X8_PIN_NONE);
+
+
 // CONFIGURATION START
 
 //set to the endpoint you would like to use
-String API = "";
+String APIROOT = "http://hw.airgradient.com/";
 
 // set to true to switch from Celcius to Fahrenheit
 boolean inF = false;
@@ -55,8 +84,16 @@ boolean connectWIFI=true;
 
 unsigned long currentMillis = 0;
 
+const int oledInterval = 5000;
+unsigned long previousOled = 0;
+
 const int sendToServerInterval = 10000;
 unsigned long previoussendToServer = 0;
+
+const int tvocInterval = 1000;
+unsigned long previousTVOC = 0;
+int TVOC = 0;
+int NOX = 0;
 
 const int co2Interval = 5000;
 unsigned long previousCo2 = 0;
@@ -81,6 +118,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Hello");
   u8g2.begin();
+  //u8g2.setDisplayRotation(U8G2_R0);
 
   EEPROM.begin(512);
   delay(500);
@@ -88,8 +126,8 @@ void setup() {
   buttonConfig = String(EEPROM.read(addr)).toInt();
   setConfig();
 
-  updateOLED2("Press Button", "Now for", "Config Menu");
-  delay(2000);
+   updateOLED2("Press Button", "Now for", "Config Menu");
+    delay(2000);
 
   currentState = digitalRead(D7);
   if (currentState == HIGH)
@@ -106,16 +144,16 @@ void setup() {
   }
 
   updateOLED2("Warming Up", "Serial Number:", String(ESP.getChipId(), HEX));
+  sgp41.begin(Wire);
   ag.CO2_Init();
   ag.PMS_Init();
   ag.TMP_RH_Init(0x44);
-  updateOLED2("Setup Success!", "Turning Off", "Display...");
-  delay(6000);
-  u8g2.clear();
 }
 
 void loop() {
   currentMillis = millis();
+  updateTVOC();
+  updateOLED();
   updateCo2();
   updatePm25();
   updateTempHum();
@@ -142,6 +180,14 @@ void inConf(){
   if (lastState == HIGH && currentState == HIGH){
      long passedDuration = millis() - pressedTime;
       if( passedDuration > 4000 ) {
+        // to do
+//        if (buttonConfig==4) {
+//          updateOLED2("Saved", "Release", "Button Now");
+//          delay(1000);
+//          updateOLED2("Starting", "CO2", "Calibration");
+//          delay(1000);
+//          Co2Calibration();
+//       } else {
           updateOLED2("Saved", "Release", "Button Now");
           delay(1000);
           updateOLED2("Rebooting", "in", "5 seconds");
@@ -150,6 +196,7 @@ void inConf(){
           EEPROM.commit();
           delay(1000);
           ESP.restart();
+ //       }
     }
 
   }
@@ -158,28 +205,33 @@ void inConf(){
   inConf();
 }
 
+
 void setConfig() {
   if (buttonConfig == 0) {
     updateOLED2("Temp. in C", "PM in ug/m3", "Display Top");
       u8g2.setDisplayRotation(U8G2_R2);
       inF = false;
       inUSAQI = false;
-  } else if (buttonConfig == 1) {
+  }
+    if (buttonConfig == 1) {
     updateOLED2("Temp. in C", "PM in US AQI", "Display Top");
       u8g2.setDisplayRotation(U8G2_R2);
       inF = false;
       inUSAQI = true;
-  } else if (buttonConfig == 2) {
+  }
+   if (buttonConfig == 2) {
     updateOLED2("Temp. in F", "PM in ug/m3", "Display Top");
       u8g2.setDisplayRotation(U8G2_R2);
       inF = true;
       inUSAQI = false;
-  } else if (buttonConfig == 3) {
+  }
+   if (buttonConfig == 3) {
     updateOLED2("Temp. in F", "PM in US AQI", "Display Top");
       u8g2.setDisplayRotation(U8G2_R2);
        inF = true;
       inUSAQI = true;
-  } else if (buttonConfig == 4) {
+  }
+    if (buttonConfig == 4) {
     updateOLED2("Temp. in C", "PM in ug/m3", "Display Bottom");
       u8g2.setDisplayRotation(U8G2_R0);
       inF = false;
@@ -190,17 +242,60 @@ void setConfig() {
       u8g2.setDisplayRotation(U8G2_R0);
       inF = false;
       inUSAQI = true;
-  } else if (buttonConfig == 6) {
+  }
+   if (buttonConfig == 6) {
     updateOLED2("Temp. in F", "PM in ug/m3", "Display Bottom");
     u8g2.setDisplayRotation(U8G2_R0);
       inF = true;
       inUSAQI = false;
-  } else  if (buttonConfig == 7) {
+  }
+   if (buttonConfig == 7) {
     updateOLED2("Temp. in F", "PM in US AQI", "Display Bottom");
       u8g2.setDisplayRotation(U8G2_R0);
        inF = true;
       inUSAQI = true;
   }
+
+
+
+  // to do
+  // if (buttonConfig == 8) {
+  //  updateOLED2("CO2", "Manual", "Calibration");
+  // }
+}
+
+void updateTVOC()
+{
+ uint16_t error;
+    char errorMessage[256];
+    uint16_t defaultRh = 0x8000;
+    uint16_t defaultT = 0x6666;
+    uint16_t srawVoc = 0;
+    uint16_t srawNox = 0;
+    uint16_t defaultCompenstaionRh = 0x8000;  // in ticks as defined by SGP41
+    uint16_t defaultCompenstaionT = 0x6666;   // in ticks as defined by SGP41
+    uint16_t compensationRh = 0;              // in ticks as defined by SGP41
+    uint16_t compensationT = 0;               // in ticks as defined by SGP41
+
+    delay(1000);
+
+    compensationT = static_cast<uint16_t>((temp + 45) * 65535 / 175);
+    compensationRh = static_cast<uint16_t>(hum * 65535 / 100);
+
+    if (conditioning_s > 0) {
+        error = sgp41.executeConditioning(compensationRh, compensationT, srawVoc);
+        conditioning_s--;
+    } else {
+        error = sgp41.measureRawSignals(compensationRh, compensationT, srawVoc,
+                                        srawNox);
+    }
+
+    if (currentMillis - previousTVOC >= tvocInterval) {
+      previousTVOC += tvocInterval;
+      TVOC = voc_algorithm.process(srawVoc);
+      NOX = nox_algorithm.process(srawNox);
+      Serial.println(String(TVOC));
+    }
 }
 
 void updateCo2()
@@ -232,6 +327,30 @@ void updateTempHum()
     }
 }
 
+void updateOLED() {
+   if (currentMillis - previousOled >= oledInterval) {
+     previousOled += oledInterval;
+
+    String ln3;
+    String ln1;
+
+    if (inUSAQI) {
+      ln1 = "AQI:" + String(PM_TO_AQI_US(pm25)) +  " CO2:" + String(Co2);
+    } else {
+      ln1 = "PM:" + String(pm25) +  " CO2:" + String(Co2);
+    }
+
+     String ln2 = "TVOC:" + String(TVOC) + " NOX:" + String(NOX);
+
+      if (inF) {
+        ln3 = "F:" + String((temp* 9 / 5) + 32) + " H:" + String(hum)+"%";
+        } else {
+        ln3 = "C:" + String(temp) + " H:" + String(hum)+"%";
+       }
+     updateOLED2(ln1, ln2, ln3);
+   }
+}
+
 void updateOLED2(String ln1, String ln2, String ln3) {
   char buf[9];
   u8g2.firstPage();
@@ -250,16 +369,19 @@ void sendToServer() {
       String payload = "{\"wifi\":" + String(WiFi.RSSI())
       + (Co2 < 0 ? "" : ", \"rco2\":" + String(Co2))
       + (pm25 < 0 ? "" : ", \"pm02\":" + String(pm25))
+      + (TVOC < 0 ? "" : ", \"tvoc_index\":" + String(TVOC))
+      + (NOX < 0 ? "" : ", \"nox_index\":" + String(NOX))
       + ", \"atmp\":" + String(temp)
       + (hum < 0 ? "" : ", \"rhum\":" + String(hum))
       + "}";
 
       if(WiFi.status()== WL_CONNECTED){
         Serial.println(payload);
-        Serial.println(API);
+        String POSTURL = APIROOT + "sensors/airgradient:" + String(ESP.getChipId(), HEX) + "/measures";
+        Serial.println(POSTURL);
         WiFiClient client;
         HTTPClient http;
-        http.begin(client, API);
+        http.begin(client, POSTURL);
         http.addHeader("content-type", "application/json");
         int httpCode = http.POST(payload);
         String response = http.getString();
@@ -276,7 +398,7 @@ void sendToServer() {
 // Wifi Manager
  void connectToWifi() {
    WiFiManager wifiManager;
-   WiFi.disconnect(); //to delete previous saved hotspot
+   //WiFi.disconnect(); //to delete previous saved hotspot
    String HOTSPOT = "AG-" + String(ESP.getChipId(), HEX);
    updateOLED2("90s to connect", "to Wifi Hotspot", HOTSPOT);
    wifiManager.setTimeout(90);
@@ -286,4 +408,17 @@ void sendToServer() {
      Serial.println("failed to connect and hit timeout");
      delay(6000);
    }
+
 }
+
+// Calculate PM2.5 US AQI
+int PM_TO_AQI_US(int pm02) {
+  if (pm02 <= 12.0) return ((50 - 0) / (12.0 - .0) * (pm02 - .0) + 0);
+  else if (pm02 <= 35.4) return ((100 - 50) / (35.4 - 12.0) * (pm02 - 12.0) + 50);
+  else if (pm02 <= 55.4) return ((150 - 100) / (55.4 - 35.4) * (pm02 - 35.4) + 100);
+  else if (pm02 <= 150.4) return ((200 - 150) / (150.4 - 55.4) * (pm02 - 55.4) + 150);
+  else if (pm02 <= 250.4) return ((300 - 200) / (250.4 - 150.4) * (pm02 - 150.4) + 200);
+  else if (pm02 <= 350.4) return ((400 - 300) / (350.4 - 250.4) * (pm02 - 250.4) + 300);
+  else if (pm02 <= 500.4) return ((500 - 400) / (500.4 - 350.4) * (pm02 - 350.4) + 400);
+  else return 500;
+};
